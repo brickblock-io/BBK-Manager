c7f5ba8e0bdad41ad3c1aa7804783c509387f2d0
Philip Paetz <philip@brickblock.io>
2019-04-16 15:18:47 +0200

    feat(ErrorBoundary): improved ErrorBoundary component

--- src/layout/components/error-boundary/index.js
+++ src/layout/components/error-boundary/index.js
@@ -4,6 +4,8 @@ import * as Sentry from '@sentry/browser'
 
 // Components
 import { Button } from '@brickblock/styleguide'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 // Types
 import type { Node } from 'react'
@@ -41,7 +43,27 @@ class ErrorBoundary extends Component<PropsT, StateT> {
     const { children } = this.props
 
     if (hasError) {
-      return <Button onClick={this.handleErrorReport}>Report feedback</Button>
+      return (
+        <Grid container justify="center">
+          <Grid item style={{ textAlign: 'center' }} xs={6}>
+            <br />
+            <Typography align="center" gutterBottom variant="h1">
+              Something went wrong&nbsp;{' '}
+              <span aria-label="Thinking face emoji" role="img">
+                🤔
+              </span>
+            </Typography>
+            <br />
+            <Typography align="center" gutterBottom variant="body1">
+              {
+                "We're sorry. This should not have happened. Our tech team has been notified about this issue. If you want to help, we'd appreciate it if you could briefly describe in 1 or 2 sentences what you did before you saw this error."
+              }
+            </Typography>
+            <br />
+            <Button onClick={this.handleErrorReport}>Report Error</Button>
+          </Grid>
+        </Grid>
+      )
     } else {
       return children
     }
