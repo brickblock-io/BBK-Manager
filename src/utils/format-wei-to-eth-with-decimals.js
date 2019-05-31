c1a57e353e27787f8c2e284f92600220a079559c
Philip Paetz <philip@brickblock.io>
2019-05-31 17:36:59 +0200

    feat(act): use “toString()” instead of “toLocaleString()” to display all decimals of ACT balance

--- src/utils/format-wei-to-eth-with-decimals.js
+++ src/utils/format-wei-to-eth-with-decimals.js
@@ -4,11 +4,11 @@ import type BN from 'bn.js'
 function formatWeiToEthWithDecimals(
   value: BN
 ): {| value: string, valueAsNumber: number |} {
-  const valueAsString = value.toString()
-  const valueAsNumber = parseInt(valueAsString) / 1e18
-  const valueAsLocalizedString = valueAsNumber.toLocaleString()
+  const valueAsStringInWei = value.toString()
+  const valueAsNumber = parseInt(valueAsStringInWei) / 1e18
+  const valueAsStringInEth = valueAsNumber.toString()
 
-  return { value: valueAsLocalizedString, valueAsNumber }
+  return { value: valueAsStringInEth, valueAsNumber }
 }
 
 export default formatWeiToEthWithDecimals
